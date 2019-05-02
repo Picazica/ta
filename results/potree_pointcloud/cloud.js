67ed57fe7569babf106979fb296f6e3009fd7146
Vitor <a67703@alunos.uminho.pt>
2019-05-02 14:05:38 +0100

    third commit

---
+++ results/potree_pointcloud/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "points": 12670180,
+    "boundingBox": {
+        "lx": 554035.03,
+        "ly": 4505755.31,
+        "lz": -18.3,
+        "ux": 554444.81,
+        "uy": 4506165.09,
+        "uz": 391.4800000000279
+    },
+    "tightBoundingBox": {
+        "lx": 554035.03,
+        "ly": 4505755.3100000009,
+        "lz": -18.3,
+        "ux": 554444.81,
+        "uy": 4506088.79,
+        "uz": 62.52
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED",
+        "CLASSIFICATION"
+    ],
+    "spacing": 2.8390390872955324,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
